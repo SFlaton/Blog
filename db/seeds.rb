8fc0b43dd48a6aca40902672a859bc0a6551d71f
Sflaton <suzanne.flaton@gmail.com>
2016-05-09 11:08:50 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.delete_all
+suzanne = User.create(email: "suzanne.flaton@gmail.com", password:"abcd1234")
+
+Post.delete_all
+Post.create(title: "My first Post", content: "Gotta start somewhere")
